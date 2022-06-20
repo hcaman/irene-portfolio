764fbfb56b5d93a4ab96207caa226536ae09bb5f
hugo cm <hugocmdesign@gmail.com>
2022-06-20 11:59:29 +0200

    create the footer with supcomps

---
+++ src/components/ui/Links.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const LinkFooter = ({name, link}) => (<a className="text-white" href={link}>{name}</a>);
+
+export const LinkFooterSocial = ({icon, link, last}) => (<a className={`btn btn-light btn-social ${last ? '' : 'mr-2'}`} href={link}><i className={icon} role="button" aria-label="Mute volume"></i></a>);
+
+export const NavLink = ({link, name}) => (<a href={link} className="nav-item nav-link active">{name}</a>);
