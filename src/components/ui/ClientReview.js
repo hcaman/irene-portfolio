1ebb1bc58144e3560a440a2031ca109b01a4b08d
hugo cm <hugocmdesign@gmail.com>
2022-06-22 15:42:39 +0200

    add item class

--- src/components/ui/ClientReview.js
+++ src/components/ui/ClientReview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const ClientReview = ({name, profession, review, image}) => (
-    <div className="text-center">
+    <div className="text-center item">
         <i className="fa fa-3x fa-quote-left text-primary mb-4"></i>
         <h4 className="font-weight-light mb-4">{review}</h4>
         <GatsbyImage className="img-fluid rounded-circle mx-auto mb-3" image={image.gatsbyImageData} alt={image.alt || image.filename || 'image'} style={{width: '80px', height: "80px"}} />
