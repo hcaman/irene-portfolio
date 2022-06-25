1730b988c8ea8529b1f4be644d0f746674126b1b
hugo cm <hugocmdesign@gmail.com>
2022-06-25 23:58:19 +0200

    Fixing third-party modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,14 @@
-// exports.createPages = async ({ actions }) => {
-//   const { createPage } = actions
-//   createPage({
-//     path: "/using-dsg",
-//     component: require.resolve("./src/templates/using-dsg.js"),
-//     context: {},
-//     defer: true,
-//   })
-// }
-// const fetch = (...args) => import(`node-fetch`).then(({ default: fetch }) => fetch(...args))
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-owl-carousel/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
